02628309491a9132de53b71c20d29757a2f51a9b
Mériadec Lantonnet <meriadeclantonnet@gmail.com>
2023-04-23 16:23:14 +0200

    [Lua] - Revert compiling `lua_setup` in DLL

--- inc/Audio.hpp
+++ inc/Audio.hpp
@@ -1,10 +1,10 @@
 #ifndef SSS_AUDIO_HPP
 #define SSS_AUDIO_HPP
 
+#include "Audio/Source.hpp"
+#include "Audio/Buffer.hpp"
 #ifdef SSS_LUA
 #include "Audio/Lua.hpp"
 #endif // SSS_LUA
-#include "Audio/Source.hpp"
-#include "Audio/Buffer.hpp"
 
 #endif // SSS_AUDIO_HPP
--- inc/Audio/Lua.hpp
+++ inc/Audio/Lua.hpp
@@ -1,12 +1,68 @@
 #ifndef SSS_AUDIO_LUA_HPP
 #define SSS_AUDIO_LUA_HPP
 
+#define SOL_ALL_SAFETIES_ON 1
+#define SOL_STRINGS_ARE_NUMBERS 1
 #include <sol/sol.hpp>
-#include "_includes.hpp"
+#include "Source.hpp"
+#include "Buffer.hpp"
 
 SSS_AUDIO_BEGIN;
 
-SSS_AUDIO_API void lua_setup_Audio(sol::state& lua);
+inline void lua_setup_Audio(sol::state& lua) try
+{
+    auto audio = lua["Audio"].get_or_create<sol::table>();
+
+    // Buffer
+    auto buffer = audio.new_usertype<Buffer>("Buffer", sol::no_constructor);
+    // Methods
+    buffer["loadFile"] = &Buffer::loadFile;
+    buffer["getProperty"] = &Buffer::getProperty;
+    buffer["id"] = sol::property(&Buffer::getID);
+    // Static functions
+    buffer["create"] = sol::overload(
+        sol::resolve<Buffer& (uint32_t)>(Buffer::create),
+        sol::resolve<Buffer& ()>(Buffer::create)
+    );
+    buffer["get"] = &Buffer::get;
+    buffer["remove"] = &Buffer::remove;
+    buffer["clearAll"] = &Buffer::clearAll;
+
+    // Source
+    auto source = audio.new_usertype<Source>("Source", sol::no_constructor);
+    // Settings
+    source["useBuffer"] = &Source::useBuffer;
+    source["queueBuffers"] = &Source::queueBuffers;
+    source["detachBuffers"] = &Source::detachBuffers;
+    // Commands
+    source["play"] = &Source::play;
+    source["pause"] = &Source::pause;
+    source["stop"] = &Source::stop;
+    // State
+    source["is_playing"] = sol::property(&Source::isPlaying);
+    source["is_paused"] = sol::property(&Source::isPaused);
+    source["is_stopped"] = sol::property(&Source::isStopped);
+    // Options
+    source["volume"] = sol::property(&Source::getVolume, &Source::setVolume);
+    source["loop"] = sol::property(&Source::isLooping, &Source::setLooping);
+    source["id"] = sol::property(&Source::getID);
+    // Static functions
+    source["create"] = sol::overload(
+        sol::resolve<Source& (uint32_t)>(Source::create),
+        sol::resolve<Source& ()>(Source::create)
+    );
+    source["get"] = &Source::get;
+    source["remove"] = &Source::remove;
+    source["clearAll"] = &Source::clearAll;
+
+    // Global properties
+    audio["getVolume"] = &getMainVolume;
+    audio["setVolume"] = &setMainVolume;
+    audio["getDevice"] = &getCurrentDevice,
+        audio["setDevice"] = &selectDevice;
+    audio["getAllDevices"] = &getDevices;
+}
+CATCH_AND_RETHROW_FUNC_EXC;
 
 SSS_AUDIO_END;
 
